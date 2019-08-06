9e1369d3e214ce350ee3c031cbfe727988b90fad
Adrian Steć <stadkr@gmail.com>
2019-08-06 06:32:49 +0200

    addMappingToSelect

--- src/main/java/pl/adrianstec/ExamApplication/controller/ThingToDoController.java
+++ src/main/java/pl/adrianstec/ExamApplication/controller/ThingToDoController.java
@@ -3,8 +3,11 @@ package pl.adrianstec.ExamApplication.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import pl.adrianstec.ExamApplication.model.Car;
 import pl.adrianstec.ExamApplication.model.ThingToDo;
 import pl.adrianstec.ExamApplication.repository.ThingToDoRepository;
 
@@ -18,6 +21,11 @@ public class ThingToDoController {
     public String create(@ModelAttribute ThingToDo thingToDo){
         thingsToDoRepository.save(thingToDo);
         return"redirect:/";
+    }
+    @GetMapping("/show")
+    public String showThings(@ModelAttribute Car car,ThingToDo thingToDo ,ModelMap map){
+        map.put("show",car.getThings());
+        return "/things";
     }
 
 }
