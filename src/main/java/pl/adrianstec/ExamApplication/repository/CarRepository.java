fcec4017cde97314da51e7b3b92920d338613b6d
Adrian Steć <stadkr@gmail.com>
2019-08-07 08:55:21 +0200

    addCarListToFindByName

--- src/main/java/pl/adrianstec/ExamApplication/repository/CarRepository.java
+++ src/main/java/pl/adrianstec/ExamApplication/repository/CarRepository.java
@@ -1,11 +1,14 @@
 package pl.adrianstec.ExamApplication.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import pl.adrianstec.ExamApplication.model.Car;
+
+import java.util.List;
 
 public interface CarRepository extends CrudRepository<Car,Integer> {
 
-   // List<Car> findCarsByName(@Param("carName") String carName);
+    List<Car> findCarsByName(@Param("carName") String carName);
 
 
 }
